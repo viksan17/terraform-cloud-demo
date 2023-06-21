0d7f7f3f42bf3807a78e2f2f8f9c7471b2662db5
viksan17 <128238642+viksan17@users.noreply.github.com>
2023-06-20 20:32:45 -0500

    created asg

--- asg/main.tf
+++ asg/main.tf
@@ -23,3 +23,69 @@ module "asg" {
   ebs_optimized     = true
   enable_monitoring = true
 }
+
+
+
+resource "aws_security_group" "lb-firewall" {
+  name        = "lb-firewall"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+
+
+resource "aws_elb" "bar" {
+  name = "foobar-terraform-elbs"
+  subnets = data.terraform_remote_state.vpc.outputs.public_subnets
+  security_groups = [
+    aws_security_group.lb-firewall.id
+  ]
+
+
+listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+}
+
+
+health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "TCP:80"
+    interval = 30
+}
+
+    cross_zone_load_balancing = true
+    idle_timeout = 400
+    connection_draining = true
+    connection_draining_timeout = 400
+}
+
+
--- vpc/output.tf
+++ vpc/output.tf
@@ -6,3 +6,12 @@ output private_subnets {
 output public_subnets {
     value = module.vpc.public_subnets
 }
+
+
+output azs {
+    value = module.vpc.azs
+}
+
+output vpc_id {
+    value = module.vpc.vpc_id
+}
